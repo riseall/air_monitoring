677e2a93c4cdc79953a7d0bd1cee75eeac11f429
Rizal Nugroho <rizalnug2@gmail.com>
2024-06-04 19:47:51 +0700

    create api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SensorDataController;
 
 Route::get('/', function () {
     return view('test');
 });
+
+Route::get('data', [SensorDataController::class, 'index']);
