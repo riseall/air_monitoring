896953d352c7b22c1675caf20aa9fe579c39ceeb
Rizal Nugroho <rizalnug2@gmail.com>
2024-06-11 18:25:21 +0700

    ganti hc jadi benzena, tambah toluent

---
+++ app/Http/Controllers/Api/SensorDataController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\SensorData;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SensorDataController extends Controller
+{
+    public function index()
+    {
+        $data = SensorData::all();
+
+        return response()->json($data, 201);
+    }
+    public function store(Request $request)
+    {
+        Log::info('Received data:', $request->all());
+
+        $validated = $request->validate([
+            'co2' => 'required|numeric',
+            'no2' => 'required|numeric',
+            'co' => 'required|numeric',
+            'benzene' => 'required|numeric',
+            'toluene' => 'required|numeric',
+            'pm25' => 'required|numeric',
+            'temperature' => 'required|numeric',
+            'humidity' => 'required|numeric',
+        ]);
+
+        try {
+            // Simpan data ke database
+            SensorData::create([
+                'tanggal' => Carbon::now(),
+                'co2' => $validated['co2'],
+                'no2' => $validated['no2'],
+                'co' => $validated['co'],
+                'benzene' => $validated['benzene'],
+                'toluene' => $validated['toluene'],
+                'pm25' => $validated['pm25'],
+                'temperature' => $validated['temperature'],
+                'humidity' => $validated['humidity'],
+            ]);
+
+            return response()->json(['message' => 'Data saved successfully'], 201);
+        } catch (\Exception $e) {
+            Log::error('Failed to save data:', ['error' => $e->getMessage()]);
+            return response()->json(['message' => 'Failed to save data'], 500);
+        }
+    }
+}
---
+++ database/migrations/2024_05_31_143236_create_sensor_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_data', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('tanggal');
+            $table->decimal('co2', 8, 2);
+            $table->decimal('no2', 8, 2);
+            $table->decimal('co', 8, 2);
+            $table->decimal('benzene', 8, 2);
+            $table->decimal('toluene', 8, 2);
+            $table->decimal('pm25', 8, 2);
+            $table->decimal('humidity', 8, 2);
+            $table->decimal('temperature', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_data');
+    }
+};
