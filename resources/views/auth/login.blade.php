f21b24effeca1c3a9338502244d39f885ef780a3
Rizal Nugroho <rizalnug2@gmail.com>
2024-06-25 05:04:02 +0700

    type submit button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                                             for="remember_me">{{ __('Remember me') }}</label>
                                     </div>
                                     <div class="text-center">
-                                        <button type="button"
+                                        <button type="submit"
                                             class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-blue-600 to-cyan-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">{{ __('Log in') }}</button>
                                     </div>
                                 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -46,7 +46,7 @@
                                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                                 </div>
                                 <div class="text-center">
-                                    <button type="button"
+                                    <button type="submit"
                                         class="inline-block w-full px-6 py-3 mt-6 mb-2 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 bg-gradient-to-tl from-gray-900 to-slate-600 hover:border-slate-700 hover:bg-slate-700 hover:text-white">{{ __('Register') }}</button>
                                 </div>
                                 <div
