467dbb2289262e83783f89ff1a2fc17bda76d54d
Rizal Nugroho <rizalnug2@gmail.com>
2024-06-28 00:30:28 +0700

    edit profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,100 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="./assets/img/air-quality.png" />
+    <title>Air Quality - Monitoring</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/navLink.js', 'resources/js/bootstrap.js', 'resources/js/breadcrumb.js'])
+    @include('layout.partial.link')
+</head>
+
+<body class="m-0 font-sans text-base antialiased font-normal leading-default bg-gray-50 text-slate-500">
+    <!-- sidenav  -->
+    @include('layout.partial.header')
+    <div class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen bg-gray-50 transition-all duration-200">
+        <nav class="absolute z-20 flex flex-wrap items-center justify-between w-full px-6 py-2 text-white transition-all shadow-none duration-250 ease-soft-in lg:flex-nowrap lg:justify-start"
+            navbar-profile navbar-scroll="true">
+            <div class="flex items-center justify-between w-full px-6 py-1 mx-auto flex-wrap-inherit">
+                <nav>
+                    <!-- breadcrumb -->
+                    <ol class="flex flex-wrap pt-1 pl-2 pr-4 mr-12 bg-transparent rounded-lg sm:mr-16">
+                        <li class="leading-normal text-sm">
+                            <a class="opacity-50" href="javascript:;">Pages</a>
+                        </li>
+                        <li class="text-sm pl-2 capitalize leading-normal before:float-left before:pr-2 before:content-['/']"
+                            aria-current="page">Profile</li>
+                    </ol>
+                    <h6 class="mb-2 ml-2 font-bold text-white capitalize">Profile</h6>
+                </nav>
+            </div>
+        </nav>
+
+        <div class="w-full px-6 mx-auto">
+            <div class="relative flex items-center p-0 mt-6 overflow-hidden bg-center bg-cover min-h-75 rounded-2xl"
+                style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%">
+                <span
+                    class="absolute inset-y-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-purple-700 to-pink-500 opacity-60"></span>
+            </div>
+            <div
+                class="relative flex flex-col flex-auto min-w-0 p-4 mx-6 -mt-16 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white/80 bg-clip-border backdrop-blur-2xl backdrop-saturate-200">
+                <div class="flex flex-wrap -mx-3">
+                    <div class="flex-none w-auto max-w-full px-3">
+                        <div
+                            class="text-base ease-soft-in-out h-18.5 w-18.5 relative inline-flex items-center justify-center rounded-xl text-white transition-all duration-200">
+                            <img src="../assets/img/bruce-mars.jpg" alt="profile_image"
+                                class="w-full shadow-soft-sm rounded-xl" />
+                        </div>
+                    </div>
+                    <div class="flex-none w-auto max-w-full px-3 my-auto">
+                        <div class="h-full">
+                            <h5 class="mb-1">{{ Auth::user()->name }}</h5>
+                            <p class="mb-0 font-semibold leading-normal text-sm">{{ Auth::user()->role }}</p>
+                        </div>
+                    </div>
+                    <div class="w-full max-w-full px-3 mx-auto mt-4 sm:my-auto sm:mr-0 md:w-1/2 md:flex-none lg:w-4/12">
+                    </div>
                 </div>
             </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
+        </div>
+        <div class="w-full p-6 mx-auto">
+            <div class="flex flex-wrap -mx-3">
+                <div class="w-full max-w-full px-3 xl:w-1/2">
+                    <div
+                        class="relative flex flex-col h-full min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-xl bg-clip-border">
+                        <div class="p-5 sm:p-8 h-full bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div class="max-w-xl">
+                                @include('profile.partials.update-profile-information-form')
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                <div class="w-full max-w-full px-3 xl:w-1/2">
+                    <div
+                        class="relative flex flex-col h-full min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-xl bg-clip-border">
+                        <div class="p-5 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div class="max-w-xl">
+                                @include('profile.partials.update-password-form')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 py-6">
+                    <div
+                        class="relative flex flex-col h-full min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-xl bg-clip-border">
+                        <div class="p-5 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                            <div class="max-w-full">
+                                @include('profile.partials.delete-user-form')
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+</body>
+@include('layout.partial.script ')
+
+</html>
