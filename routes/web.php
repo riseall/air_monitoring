dea4fc7aa25ef8feb1ecbc456f4ce4b8d28bb89f
Rizal Nugroho <rizalnug2@gmail.com>
2024-07-01 21:18:01 +0700

    route fix

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,35 @@
 
 use App\Http\Controllers\Api\SensorDataController;
 use App\Http\Controllers\ChartController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/user', function () {
-    return view('user-das');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/user', function () {
+    return view('user');
+});
+
 Route::get('/kualitas', function () {
     return view('kualitas');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,12 +38,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/user-data', [UserController::class, 'getUserData'])->name('user.data');
 Route::get('/data', [SensorDataController::class, 'index'])->name('data');
-Route::get('/user', [SensorDataController::class, 'newData'])->name('new.data');
+Route::get('/dashboard', [SensorDataController::class, 'newData'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/chart1/data', [ChartController::class, 'getBar'])->name('chart1.data');
 Route::get('/chart/data', [ChartController::class, 'getData'])->name('chart.data');
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
-Route::get('/user', [NewsController::class, 'index'])->name('news.index');
-Route::get('/news/news', [NewsController::class, 'search'])->name('news.search');
+Route::get('/', [NewsController::class, 'index'])->name('welcome');
 
 require __DIR__ . '/auth.php';
